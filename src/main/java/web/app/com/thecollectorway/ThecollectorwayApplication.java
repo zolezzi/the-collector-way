47c3830ea650c7955b9a0f86d7f3ddaa61212dbe
zolezzi <cezcardozo23@gmail.com>
2023-08-14 23:40:20 -0300

    Adding project skeleton

---
+++ src/main/java/web/app/com/thecollectorway/ThecollectorwayApplication.java
@@ -0,0 +1,13 @@
+package web.app.com.thecollectorway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThecollectorwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThecollectorwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/web/app/com/thecollectorway/ThecollectorwayApplicationTests.java
@@ -0,0 +1,13 @@
+package web.app.com.thecollectorway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThecollectorwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
